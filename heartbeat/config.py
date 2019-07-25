55658f595ce4b9a69c99659450500ca73620596a
digitamo <osama.khaled.saad@gmail.com>
2019-07-25 03:52:46 +0200

    fix(heartbeat): production config

--- heartbeat/config.py
+++ heartbeat/config.py
@@ -13,13 +13,18 @@ class ProductionConfig:
     #                                                         'sqlite:////' + os.path.join(BASE_DIR, 'data.sqlite')))
     # NOTE: Uncomment this when using postgres.
     # SQLALCHEMY_DATABASE_URI = f'postgres://{USER}:{PASSWORD}@db:5432/users'
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
+    with open(os.environ.get('POSTGRES_USER_FILE'), 'r') as file:
+        POSTGRES_USER = file.read()
+    with open(os.environ.get('POSTGRES_PASSWORD_FILE'), 'r') as file:
+        POSTGRES_PASSWORD = file.read()
+    with open(os.environ.get('POSTGRES_DB_FILE'), 'r') as file:
+        POSTGRES_DB = file.read()
+        
     DB_HOST = 'postgres'
     DB_PORT = '5432'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(user=POSTGRES_USER,
-                                                                                                passwd=POSTGRES_PASSWORD,
-                                                                                                host=DB_HOST,
-                                                                                                port=DB_PORT,
-                                                                                                db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
+        user=POSTGRES_USER,
+        passwd=POSTGRES_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        db=POSTGRES_DB)
