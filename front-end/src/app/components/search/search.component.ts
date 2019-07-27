f15c63193fd2f30e9123e182a01e37b5621ba31b
digitamo <osama.khaled.saad@gmail.com>
2019-07-27 15:24:56 +0200

    test(front-end): add integration test

--- front-end/src/app/components/search/search.component.ts
+++ front-end/src/app/components/search/search.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ApiService} from '../../services/api.service';
 import {MatTableDataSource} from '@angular/material';
 import {Vehicle} from '../../models/vehicle';
+import Timer = NodeJS.Timer;
 
 @Component({
   selector: 'app-search',
@@ -14,7 +15,7 @@ export class SearchComponent implements OnInit, OnDestroy {
   public online: string;
   public displayedColumns: string[] = ['id', 'reg-no', 'customer', 'online'];
 
-  private readonly intervalId: number;
+  private readonly intervalId: Timer;
 
   constructor(private apiService: ApiService) {
     this.intervalId = setInterval(() => this.applyFilter(), 60000)
